a8399b16839a211151738b095db614e78d26dbf0
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2023-11-21 22:38:38 +0800

    Auth setup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
+        try {
+            $payload = $request->all();
+            if (
+                !Auth::attempt([
+                    'mobile_number' => $payload['username'],
+                    'password' => $payload['password'],
+                ])
+            ) {
+                return customResponse()
+                    ->data([])
+                    ->message('Invalid credentials.')
+                    ->unathorized()
+                    ->generate();
+            }
+            $accessToken = Auth::user()->createToken('authToken')->accessToken;
+            $user = User::find(Auth::id());
+            return customResponse()
+                ->data([
+                    'access_token' => $accessToken,
+                    'user' => $user,
+                ])
+                ->message('Done')
+                ->success()
+                ->generate();
+        } catch (\Exception $e) {
+            return customResponse()
+                ->data([])
+                ->message($e->getMessage())
+                ->success()
+                ->generate();
+        }
     }
 
     public function register(RegisterRequest $request)
     {
         try {
+            $payload = $request->all();
+            $user = User::create([
+                'first_name' => trim($payload['first_name']),
+                'last_name' => trim($payload['last_name']),
+                'birthday' => $payload['birthday'],
+                '' => trim($payload['mobile_number']),
+                'password' => $payload['password'],
+                'level' => $payload['level'],
+            ]);
             return customResponse()
-                ->data($request->all())
-                ->message('Done')
+                ->data($user)
+                ->message('Operation success.')
                 ->success()
                 ->generate();
         } catch (\Exception $e) {
---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function sampleFunction(Request $request)
+    {
+        try {
+            return customResponse()
+                ->data($request->all())
+                ->message('')
+                ->success()
+                ->generate();
+        } catch (\Exception $e) {
+            return customResponse()
+                ->data([])
+                ->message($e->getMessage())
+                ->success()
+                ->generate();
+        }
+    }
+}
