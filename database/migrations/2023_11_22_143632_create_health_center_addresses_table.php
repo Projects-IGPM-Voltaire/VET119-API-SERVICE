91b39cb4a643e02cabaa6b68f8a98c64af5d42cb
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2023-11-22 22:51:11 +0800

    WIP

---
+++ database/migrations/2023_11_22_143632_create_health_center_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('health_center_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('city_code');
+            $table
+                ->foreign('city_code')
+                ->references('code')
+                ->on('cities')
+                ->onDelete('cascade');
+            $table->string('barangay_code');
+            $table
+                ->foreign('barangay_code')
+                ->references('code')
+                ->on('barangays')
+                ->onDelete('cascade');
+            $table->string('house_number')->nullable();
+            $table->string('street')->nullable();
+            $table->string('map_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('health_center_addresses');
+    }
+};
