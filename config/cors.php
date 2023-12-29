779051afe3d22b103732bb664e5deb2c4d744b47
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2023-12-29 20:39:40 +0800

    configure cors

--- config/cors.php
+++ config/cors.php
@@ -2,8 +2,12 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*', 'OPTIONS'],
-    'allowed_origins' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        '*',
+        'https://mediqueue.vercel.app',
+        'http://mediqueue.vercel.app',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
