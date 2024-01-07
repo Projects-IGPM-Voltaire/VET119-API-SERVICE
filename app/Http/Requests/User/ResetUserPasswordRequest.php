db0ba388ffa8f00902e550f51be7d8930008e3b1
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2024-01-07 14:26:31 +0800

    Reset password API

--- app/Http/Requests/User/ResetUserPasswordRequest.php
+++ app/Http/Requests/User/ResetUserPasswordRequest.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Requests\User;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\FormRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
 
 class ResetUserPasswordRequest extends FormRequest
 {
@@ -11,13 +12,13 @@ class ResetUserPasswordRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
