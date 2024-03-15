bc34e869d6c8bb908ff2ab3cfaea501c24c594f3
chesterrey <rey.tan@student.ateneo.edu>
2024-03-15 16:39:34 +0800

    added appointment model and migration

---
+++ database/migrations/2024_03_15_163345_create_appointments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->date('date');
+            $table->time('time_from');
+            $table->time('time_to');
+            $table->string('patient_number');
+            $table->string('reference_number');
+            $table->timestamps();
+
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
