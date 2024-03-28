fcf9e6ab2e53613021763d8f0d3eca19942d5612
chesterrey <rey.tan@student.ateneo.edu>
2024-03-28 14:19:14 +0800

    debug

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -40,8 +40,7 @@ class AuthServiceProvider extends ServiceProvider
 
             return (new MailMessage)
                 ->subject('Verify Email Address')
-                ->line('Click the button below to verify your email address.' . $apiUrl
-                )
+                ->line('Click the button below to verify your email address.')
                 ->action('Verify Email Address', $url);
         });
     }
